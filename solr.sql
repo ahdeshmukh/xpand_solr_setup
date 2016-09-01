d87f92f60b787f11a0f087f1e70e15ec02aee352
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2016-08-31 22:06:54 -0400

    adding updated_time for DIH

--- solr.sql
+++ solr.sql
@@ -7,10 +7,11 @@ CREATE TABLE person(
 	firstname VARCHAR(25), 
 	lastname VARCHAR(25), 
 	description TEXT,
-	created DATETIME,
+	created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
 
-INSERT INTO person VALUES(NULL, 'Amit', 'Deshmukh', 'Amit works as a software engineer at Xpand Corporation. He is married and lives in Leesburg.', NOW());
-INSERT INTO person VALUES(NULL, 'Payal', 'Sureja', 'Payal works as a software engineer at Xpand Corporation. She is married and lives in Herndon. She has 1 kid.', NOW());
-INSERT INTO person VALUES(NULL, 'Chris', 'Cassad', 'Chris works as a manager and principal software engineer at Xpand Corporation. He is married and lives in Aldie. He has 2 kids', NOW());
+INSERT INTO person VALUES(NULL, 'Amit', 'Deshmukh', 'Amit works as a software engineer at Xpand Corporation. He is married and lives in Leesburg.', NULL, NULL);
+INSERT INTO person VALUES(NULL, 'Payal', 'Sureja', 'Payal works as a software engineer at Xpand Corporation. She is married and lives in Herndon. She has 1 kid.', NULL, NULL);
+INSERT INTO person VALUES(NULL, 'Chris', 'Cassad', 'Chris works as a manager and principal software engineer at Xpand Corporation. He is married and lives in Aldie. He has 2 kids', NULL, NULL);
